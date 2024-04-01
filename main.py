9a32d44e17ed13b5d8fb7c8684dc653dd1286688
Greg Clinton <greg.clinton@gmail.com>
2024-04-01 11:34:45 -0700

    latest

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+keys = dict(line.split(',') for line in open('api_keys').read().splitlines())
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory=".", html=True), name="static")
+
+from openai import OpenAI
+
+client = OpenAI(
+    api_key = keys['OPENAI_API_KEY'],
+)
+
+completion = client.chat.completions.create(
+#   openai.com/pricing                context   $in/$out/M    trained
+#   model = 'gpt-4-0125-preview',  #    128K       10/30      Dec '23
+#   model = 'gpt-4',               #      8K       30/60      Sep '21
+#   model = 'gpt-4-32k',           #     32K       60/120     Sep '21
+    model = 'gpt-3.5-turbo-0125',  #      4K      0.5/1.5     Sep '21
+    messages = [
+        {"role": "system", "content": "You are a poetic assistant."},
+        {"role": "user", "content": "Compose a haiku that explains relativity."}
+    ]
+)
+
+haiku = completion.choices[0].message.content
+
+@app.get('/')
+async def read_test(test: str):
+    return haiku
