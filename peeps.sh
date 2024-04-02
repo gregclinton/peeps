77fb3af82cb954d1cd00bceb453328dfe1ed40b8
Greg Clinton <greg.clinton@gmail.com>
2024-04-02 09:47:13 -0700

    latest

--- peeps.sh
+++ peeps.sh
@@ -1,1 +1,1 @@
-uvicorn main:app --host 0.0.0.0 --port 443 --ssl-keyfile=/etc/ssl/private/key.pem --ssl-certfile=/etc/ssl/certs/cert.pem --reload
+uvicorn main:app --host 0.0.0.0 --port 443 --ssl-keyfile=/etc/ssl/private/key.pem --ssl-certfile=/etc/ssl/certs/cert.pem
