8dfcbce79d874a984ff648b2e19b5e068d6fb50b
Greg Clinton <greg.clinton@gmail.com>
2024-04-03 08:37:03 -0700

    latest

--- static/speech.js
+++ static/speech.js
@@ -1,13 +1,26 @@
-let recorder;
-let recorderStream;
-let recording = false;
+const recorder = {
+    audio: 0,
+    stream: 0,
+    recording: false,
 
-function record() {
-    if (recording) {
-        recorder.stopRecording(() => {
+    start: () => {
+        recorder.audio = new RecordRTC(recorder.stream, {
+            mimeType: 'audio/wav',
+            timeSlice: 1000,
+            recorderType: RecordRTC.StereoAudioRecorder,
+            numberOfAudioChannels: 1,
+            audioBitsPerSecond: 128000
+        });
+
+        recorder.audio.startRecording();
+        recorder.recording = true;
+    },
+
+    send: () => {
+        recorder.audio.stopRecording(() => {
             const data = new FormData();
 
-            data.append("file", recorder.getBlob(), 'audio/prompt.wav');
+            data.append("file", recorder.audio.getBlob(), 'audio/prompt.wav');
 
             fetch('/chat/', {
                 method: 'POST',
@@ -18,22 +31,16 @@ function record() {
                 new Audio(URL.createObjectURL(blob)).play();
             });
         });
-        recording = false;
-    } else {
-        recorder = new RecordRTC(recorderStream, {
-            mimeType: 'audio/wav',
-            timeSlice: 1000,
-            recorderType: RecordRTC.StereoAudioRecorder,
-            numberOfAudioChannels: 1,
-            audioBitsPerSecond: 128000
-        });
+        recorder.recording = false;
+    },
 
-        recorder.startRecording();
-        recording = true;
+    cancel: () => {
+        recorder.audio_.stopRecording();
+        recorder.recording = false;
     }
-}
+};
 
 navigator.mediaDevices.getUserMedia({ audio: true })
 .then(stream => {
-    recorderStream = stream;
+    recorder.stream = stream;
 })
