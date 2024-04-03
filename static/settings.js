f7e8e2b7dc22e22d4771c046cbfa955f49f1e90d
Greg Clinton <greg.clinton@gmail.com>
2024-04-03 14:31:38 -0700

    latest

--- static/settings.js
+++ static/settings.js
@@ -4,36 +4,36 @@ settings = {
         e.hidden = !e.hidden;
     },
 
-    model: 'gpt-3.5-turbo-0125',
+    name: 'gpt-3.5-turbo-0125',
 
     companies: [
         {
-        company: "openai",
+        name: "openai",
         url: "https://openai.com/pricing",
         models: [
             {
-            model: "gpt-4-0125-preview",
+            name: "gpt-4-0125-preview",
             context: 128000,
             inPrice: 10,
             outPrice: 30,
             trainingDate: "Dec '23"
             },
             {
-            model: "gpt-4",
+            name: "gpt-4",
             context: 8000,
             inPrice: 30,
             outPrice: 60,
             trainingDate: "Sep '21"
             },
             {
-            model: "gpt-4-32k",
+            name: "gpt-4-32k",
             context: 32000,
             inPrice: 60,
             outPrice: 120,
             trainingDate: "Sep '21"
             },
             {
-            model: "gpt-3.5-turbo-0125",
+            name: "gpt-3.5-turbo-0125",
             context: 4000,
             inPrice: 0.5,
             outPrice: 1.5,
@@ -42,25 +42,25 @@ settings = {
         ]
         },
         {
-        company: "anthropic",
+        name: "anthropic",
         url: "https://anthropic.com/news/claude-3-family",
         models: [
             {
-            model: "claude-3-haiku-20240307",
+            name: "claude-3-haiku-20240307",
             context: null,
             inPrice: 0.25,
             outPrice: 1.25,
             trainingDate: null
             },
             {
-            model: "claude-3-sonnet-20240229",
+            name: "claude-3-sonnet-20240229",
             context: null,
             inPrice: 3,
             outPrice: 15,
             trainingDate: null
             },
             {
-            model: "claude-3-opus-20240229",
+            name: "claude-3-opus-20240229",
             context: null,
             inPrice: 15,
             outPrice: 75,
@@ -69,25 +69,25 @@ settings = {
         ]
         },
         {
-        company: "google",
+        name: "google",
         url: "https://ai.google.dev/models/gemini",
         models: [
             {
-            model: "gemini-1.0-pro-001",
+            name: "gemini-1.0-pro-001",
             context: 30720,
             inPrice: "free",
             outPrice: 60,
             trainingDate: "Feb '24"
             },
             {
-            model: "gemini-1.5-pro-latest",
+            name: "gemini-1.5-pro-latest",
             context: 1048576,
             inPrice: "free",
             outPrice: 2,
             trainingDate: "Feb '24"
             },
             {
-            model: "gemini-nano",
+            name: "gemini-nano",
             context: null,
             inPrice: 0.13,
             outPrice: 0.38,
@@ -96,25 +96,25 @@ settings = {
         ]
         },
         {
-        company: "mistralai",
+        name: "mistralai",
         url: "https://docs.mistral.ai/platform/pricing",
         models: [
             {
-            model: "mistral-small-latest",
+            name: "mistral-small-latest",
             context: null,
             inPrice: 2,
             outPrice: 6,
             trainingDate: null
             },
             {
-            model: "mistral-medium-latest",
+            name: "mistral-medium-latest",
             context: null,
             inPrice: 2.7,
             outPrice: 8.1,
             trainingDate: null
             },
             {
-            model: "mistral-large-latest",
+            name: "mistral-large-latest",
             context: null,
             inPrice: 8,
             outPrice: 24,
@@ -136,8 +136,40 @@ window.onload = () => {
 
     settings.companies.forEach(company => {
         const div = document.createElement('div');
+        const span = document.createElement('span');
 
-        div.innerHTML = '<h3>' + company.company + '</h3><br/>';
+        span.innerHTML = '<h3>' + company.name + '</h3><br/>';
+        div.appendChild(span);
+        const table = document.createElement('table');
+        const tbody = document.createElement('tbody');
+        const thead = document.createElement('thead');
+
+        thead.innerHTML = '<th>model</th><th>context</th><th>in</th><th>out</th><th>trained</th>'
+        div.appendChild(table);
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        div.appendChild(table);
+
+        company.models.forEach(model => {
+            const tr = document.createElement('tr');
+            const addTd = (value => {
+                const td = document.createElement('td');
+
+                if (value > 1000) {
+                    value =  Math.round(value / 1000) + 'K';
+                }
+                td.innerHTML = value;
+                tr.appendChild(td)
+            });
+
+            addTd(model.name);
+            addTd(model.context);
+            addTd(model.inPrice);
+            addTd(model.outPrice);
+            addTd(model.trainingDate);
+            tbody.appendChild(tr);
+        });
+
         models.appendChild(div);
     });
 };
