c10e552a3eaf7cb7c2284179c057204baf00981b
Greg Clinton <greg.clinton@gmail.com>
2024-04-03 06:25:47 -0700

    latest

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from chat import process
 from speech import stt, tts
+from pathlib import Path
 import shutil
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory = "static"), name ="static")
 
-@app.get('/', response_class = HTMLResponse)
-async def read():
-    return open('index.html').read()
+@app.get("/")
+async def get_index():
+    return FileResponse(Path("static/index.html"))
 
 @app.post("/chat/")
 async def post_to_chat(file: UploadFile = File(...)):
