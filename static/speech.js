f87438a9a8802a6d2b63db1cc497fc9a25b08dd4
Greg Clinton <greg.clinton@gmail.com>
2024-04-03 06:45:18 -0700

    latest

---
+++ static/speech.js
@@ -0,0 +1,36 @@
+let recorder;
+
+function record() {
+    if (recorder.recording) {
+        recorder.stopRecording(() => {
+            const data = new FormData();
+
+            data.append("file", recorder.getBlob(), 'recorder.wav');
+
+            fetch('/chat/', {
+                method: 'POST',
+                body: data,
+            })
+            .then(response => response.blob())
+            .then(blob => {
+                new Audio(URL.createObjectURL(blob)).play();
+            });
+        });
+        recorder.recording = false;
+        document.getElementById('main').innerHTML = '';
+    } else {
+        recorder.startRecording();
+        recorder.recording = true;
+    }
+}
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+.then(stream => {
+    recorder = new RecordRTC(stream, {
+        mimeType: 'audio/wav',
+        timeSlice: 1000,
+        recorderType: RecordRTC.StereoAudioRecorder,
+        numberOfAudioChannels: 1,
+        audioBitsPerSecond: 128000
+    });
+})
