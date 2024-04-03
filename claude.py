6fd282255f3db0a0f9d5d4cace1ba6b0369806cb
Greg Clinton <greg.clinton@gmail.com>
2024-04-03 12:10:25 -0700

    latest

--- claude.py
+++ claude.py
@@ -5,10 +5,7 @@ client = anthropic.Anthropic(api_key = secrets.get('ANTHROPIC_API_KEY'))
 
 def prompt(messages):
     return client.messages.create(
-    #   anthropic.com/news/claude-3-family      context   $in/$out/M    trained
-        model = 'claude-3-haiku-20240307',   #            0.25/1.25
-    #   model = 'claude-3-sonnet-20240229',  #               3/15
-    #   model = 'claude-3-opus-20240229',    #              15/75
+        model = settings.model,
         max_tokens = 1000,
         temperature = 0.0,
         system = "You are a helpful assistant.",
--- mistral.py
+++ mistral.py
@@ -6,9 +6,6 @@ client = MistralClient(api_key = secrets.get('MISTRAL_API_KEY'))
 from mistralai.models.chat_completion import ChatMessage
 
 chat_response = client.chat(
-# docs.mistral.ai/platform/pricing          $in/$out/M
-     model = 'mistral-small-latest',   #        2/6
-#   model = 'mistral-medium-latest',   #      2.7/8.1
-#   model = 'mistral-large-latest',    #        8/24
+    model = settings.model
     messages=[ChatMessage(role="user", content="Write a haiku about relativity.")],
 )
