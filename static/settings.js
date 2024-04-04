73fb0fe836d186f6bf7f45f4e4b294596a8a74cd
Greg Clinton <greg.clinton@gmail.com>
2024-04-04 07:03:12 -0700

    latest

--- static/settings.js
+++ static/settings.js
@@ -5,21 +5,6 @@ settings = {
     },
 
     name: 'gpt-3.5-turbo-0125',
-
-    models: [
-        ['gpt-4-0125-preview', '$10/$30'],
-        ['gpt-4', '$10/$60'],
-        ['gpt-3.5-turbo-0125', '$0.50/$1.50'],
-        ['claude-3-haiku-20240307' , '$0.25/$1.25'],
-        ['claude-3-sonnet-20240229', '$3/$15'],
-        ['claude-3-opus-20240229', '$15/$75'],
-        ['gemini-1.0-pro-001', 'free'],
-        ['gemini-1.5-pro-latest', 'free'],
-        ['gemini-nano', '$0.13/$0.38'],
-        ['mistral-small-latest' , '$2/$6'],
-        ['mistral-medium-latest', '$2.70/$8.10'],
-        ['mistral-large-latest', '$8/24'],
-    ]
 }
 
 fetch('/settings/', {
@@ -42,7 +27,20 @@ window.onload = () => {
     table.appendChild(tbody);
     models.appendChild(table);
 
-    settings.models.forEach(model => {
+    [
+        ['gpt-4-0125-preview', '$10/$30'],
+        ['gpt-4', '$10/$60'],
+        ['gpt-3.5-turbo-0125', '$0.50/$1.50'],
+        ['claude-3-haiku-20240307' , '$0.25/$1.25'],
+        ['claude-3-sonnet-20240229', '$3/$15'],
+        ['claude-3-opus-20240229', '$15/$75'],
+        ['gemini-1.0-pro-001', 'free'],
+        ['gemini-1.5-pro-latest', 'free'],
+        ['gemini-nano', '$0.13/$0.38'],
+        ['mistral-small-latest' , '$2/$6'],
+        ['mistral-medium-latest', '$2.70/$8.10'],
+        ['mistral-large-latest', '$8/24'],
+    ].forEach(model => {
         const tr = document.createElement('tr');
         const addTd = (value => {
             if (value !== null) {
