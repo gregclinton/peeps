28ea1fd6c400eed4bbd6cb065befd3b04bdc8348
Greg Clinton <greg.clinton@gmail.com>
2024-04-05 13:42:22 -0700

    latest

--- gemini.py
+++ gemini.py
@@ -4,11 +4,5 @@ import secrets
 genai.configure(api_key = secrets.get('GEMINI_API_KEY'))
 
 def respond(chat, model):
-    transcript = ''
-
-    for item in chat:
-        for k, v in item.items():
-            transcript += k + ': ' + v
-
-    transcript += 'response: '
+    transcript = "\n".join(f"{k}: {v}" for d in chat for k, v in d.items()) + '\response: '
     return genai.GenerativeModel(model).generate_content(transcript).text
