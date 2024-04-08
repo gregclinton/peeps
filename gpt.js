3ebd2530af2b6fc3b456d368d4a86f0a1740fef3
Greg Clinton <greg.clinton@gmail.com>
2024-04-08 10:20:10 -0700

    latest

--- gpt.js
+++ gpt.js
@@ -1,17 +1,4 @@
-console.log('hi hi hi');
-
-/*
-
-curl https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0.7
-   }'
-
-*/
+/// https://platform.openai.com/docs/api-reference/introduction
 
 fetch('/openai/v1/chat/completions', {
     method: 'POST',
