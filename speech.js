2b293c383db2f5ce5ebc3fa12417d22fe50635e4
Greg Clinton <greg.clinton@gmail.com>
2024-04-08 12:13:26 -0700

    latest

--- speech.js
+++ speech.js
@@ -9,7 +9,7 @@ const speech = {
         const data = new FormData();
 
         data.append('file', blob, 'audio/stt.wav');
-        data.append('model', 'whisper-t'); // 100 seconds for a penny
+        data.append('model', 'whisper-1'); // 100 seconds for a penny
         data.append('language', 'en'); // optional but improves accuracy and latency
         data.append('response_format', 'text');
 
