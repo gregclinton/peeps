bdd0ca785542612d6ee0b0767846734dc6b32764
Greg Clinton <greg.clinton@gmail.com>
2024-04-13 10:11:39 -0700

    latest

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ let settings = { model: 'gemini', temperature: 50 };
 
 peeps.register('Alfred',
 `
- Provide as many of the following keys as mentioned by the prompt:
-     model (gpt, claude, gemini or mistral),
-     temperature (between 0 (coldest) and 100 (hottest) inclusive),
- If a key is not mentioned or hinted at, omit that key entirely.
+Provide as many of the following keys as mentioned by the prompt:
+    model (gpt, claude, gemini or mistral),
+    temperature (between 0 (coldest) and 100 (hottest) inclusive),
+If a key is not mentioned or hinted at, omit that key entirely.
 `, 'none', o => {
     settings = {...settings, ...o};
     delete settings.reply;
